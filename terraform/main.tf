9eaf174b3ef1b0f309b6fef6a1a79dfe50126655
cosmin1230 <cosminel1230@hotmail.com>
2025-04-23 13:38:00 +0300

    Terraformfix

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,6 +137,7 @@ resource "google_container_node_pool" "nodes" {
   node_config {
     machine_type = "e2-medium"
     disk_size_gb = 30
+    disk_type = "pd-standard"
     oauth_scopes = [
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
