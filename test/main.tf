0f08144a696158ebf764f385b0ca206274d971fd
cosmin1230 <cosminel1230@hotmail.com>
2025-05-02 12:40:22 +0300

    Files for review

--- test/main.tf
+++ test/main.tf
@@ -115,7 +115,7 @@ resource "aws_route" "private_route" {
 resource "aws_route_table_association" "public_subnet_association" {
   count = 2
   subnet_id      = aws_subnet.public_subnet[count.index].id
-  route_table_id = aws_route_table.public_route_table[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
 } 
 
 # Associate the private route table with the private subnet
--- test/outputs.tf
+++ test/outputs.tf
@@ -6,15 +6,20 @@ output "vpc_id" {
 
 output "public_subnet_id" {
   description = "The ID of the public subnet"
-  value       = aws_subnet.public_subnet.id
+  value       = aws_subnet.public_subnet[*].id
 }
 
 output "private_subnet_id" {
   description = "The ID of the private subnet"
-  value       = aws_subnet.private_subnet.id
+  value       = aws_subnet.private_subnet[*].id
 }
 
 output "nat_gateway_id" {
   description = "The ID of the NAT Gateway"
-  value       = aws_nat_gateway.nat.id
+  value       = aws_nat_gateway.my_nat_gateway[*].id
+}
+
+output "nat_eip_public_ips" {
+  description = "List of Elastic IPs for NAT Gateways"
+  value       = aws_eip.nat_eip[*].public_ip
 }
